9fd4dbea9dbc42c445aff60004444545fd57cfe2
itselectroz <47079216+itselectroz@users.noreply.github.com>
2019-07-01 20:08:05 +0100

    Advanced algorithm to discrete fourier transform

--- sketch.js
+++ sketch.js
@@ -1,10 +1,53 @@
-function setup() {
-	createCanvas(windowWidth, windowHeight);
-}
 
 let t = 0;
 
 let path = [];
+let circles = [];
+
+function fourierTransform(values)
+{
+	let returns = [];
+	
+	let N = values.length;
+
+	for(let k = 0; k < N; ++k)
+	{
+		let real = 0;
+		let imag = 0;
+
+		for(let n = 0; n < N; ++n)
+		{
+			let angle = (TWO_PI*k*n)/N;
+			real += values[n] * cos(angle);
+			imag += values[n] * sin(angle);
+		}
+
+		real = real / N;
+		imag = imag / N;
+
+		let freq = k;
+		let amp = sqrt(pow(real, 2) + pow(imag, 2));
+		let phase = atan2(imag, real);
+
+		returns.push({
+			re: real,
+			im: imag,
+			freq: freq,
+			amp: amp,
+			phase: phase
+		});
+	}
+
+	returns.sort((x,y) => y.amp - x.amp);
+
+	return returns;
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	circles = fourierTransform([100, -100, 100, -100, 100, -100, 100, -100, -100]);
+	console.log(circles);
+}
 
 function draw() {
 	background(0);
@@ -15,18 +58,18 @@ function draw() {
 
 	let px = py = 0;
 
-	let n = 200;
+	let n = circles.length;
 
 	for(let i = 0; i < n; i++)
 	{
-		let v = 2*i + 1;
+		let circle = circles[i];
 
-		let r = (4/(v*PI))*400/2;
+		let r = circle.amp;
 
 		ellipse(px,py, r*2, r*2);
 
-		let x = px + r*cos(v*t);
-		let y = py + r*sin(v*t);
+		let x = px + r*cos(circle.freq * t + circle.phase);
+		let y = py + r*sin(circle.freq * t + circle.phase);
 
 		line(px, py, x, y);
 
